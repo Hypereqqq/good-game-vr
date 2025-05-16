e0012fd3c7b4105399b3b9109810479c227dc84a
Patryk Kaczmarczyk <patrykkkaczma@wp.pl>
2025-05-16 14:40:00 +0200

    init

---
+++ src/pages/AdminReservations.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AdminReservations: React.FC = () => {
+
+  return (
+    <section className="bg-[#0f1525] text-white px-6 py-16 min-h-screen">
+      <h1 className="text-3xl font-bold text-[#00d9ff] text-center uppercase">
+        Panel Administratora - REZERWACJE
+      </h1>
+    </section>
+  );
+};
+
+export default AdminReservations;
